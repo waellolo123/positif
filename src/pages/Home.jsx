c7c2a6cb1e12746a1d44e3882951ec88cb6ea06e
wael <wael.22@live.fr>
2025-11-22 09:42:51 +0100

    adding images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,10 @@ import Formulaire from "../components/Formulaire";
 import ImageDetails from "../components/ImageDetails";
 import Text from "../components/Text";
 import backgr from "/back.png";
+import child1 from "../assets/child1.jpeg";
+import child2 from "../assets/child2.jpeg";
+import child3 from "../assets/child3.jpeg";
+import child4 from "../assets/child4.jpeg";
 
 const Home = () => {
   return (
@@ -21,8 +25,11 @@ const Home = () => {
       <p className="mt-2 text-gray w-[80%] max-sm:w-full">
        Nos joggings pour enfants allient confort, résistance et style. Confectionnés dans des tissus doux et respirants, ils offrent une liberté de mouvement idéale pour le quotidien, l’école ou les activités sportives. Grâce à des finitions solides et des matières durables, ils résistent aux lavages fréquents tout en gardant leurs couleurs éclatantes. Pratiques et modernes, nos joggings assurent aux enfants un look tendance et une aisance incomparable, jour après jour. 
       </p>
-      <div className="">
-
+      <div className="flex gap-4 items-center justify-center mt-10 flex-wrap">
+       <img src={child1} alt="" className="w-[300px] h-[400px] object-cover"/> 
+       <img src={child2} alt="" className="w-[300px] h-[400px] object-cover"/> 
+       <img src={child3} alt="" className="w-[300px] h-[400px] object-cover"/> 
+       <img src={child4} alt="" className="w-[300px] h-[400px] object-cover"/> 
       </div>
      </div>
 
