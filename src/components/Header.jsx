5aad915a11217af715b4fb56437e9e9485656212
wael <wael.22@live.fr>
2025-11-26 09:33:25 +0100

    lasts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,11 +17,11 @@ const Header = () => {
      <div className="max-sm:mt-5 flex items-center justify-center gap-5">
       <div className="flex items-center justify-center gap-2 text-white">
         <FaPhone className="size-4 max-sm:text-amber-500"/>
-         <span className="text-sm max-sm:text-amber-500">0021622555000</span>
+         <span className="text-sm max-sm:text-amber-500">24232329</span>
       </div>
       <div className="flex items-center justify-center gap-2 text-white">
         <IoIosMail className="size-5 max-sm:text-amber-500"/>
-        <span className="text-sm max-sm:text-amber-500">positif@gmail.com</span>
+        <span className="text-sm max-sm:text-amber-500">Positif.shopping@gmail.com</span>
       </div>
      </div>
     </div>
